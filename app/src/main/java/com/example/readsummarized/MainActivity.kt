c8dd50602b6a1c868d72ccefa492534486a98b20
matveevaolga <matveevaolga2882@gmail.com>
2024-11-11 01:31:27 +0300

    Letting the user to choose a file to read and summarize and changing user's permissions

--- app/src/main/java/com/example/readsummarized/MainActivity.kt
+++ app/src/main/java/com/example/readsummarized/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.readsummarized
 
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.webkit.MimeTypeMap
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,17 +19,24 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.readsummarized.network.TextSummarizer
 import com.example.readsummarized.ui.theme.ReadSummarizedTheme
-
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import java.io.File
 
 class MainActivity : ComponentActivity() {
     private val textSummarizer = TextSummarizer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        fetchPdfFiles()
 
         setContent {
             ReadSummarizedTheme {
@@ -50,64 +57,53 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
 
-    //Method for getting all pdf files from device for all android versions
-    protected fun pdfFiles(): ArrayList<String> {
-        val pdfList = ArrayList<String>()
-
-        val projection = arrayOf(
-            MediaStore.Files.FileColumns.DISPLAY_NAME,
-            MediaStore.Files.FileColumns.DATE_ADDED,
-            MediaStore.Files.FileColumns.DATA,
-            MediaStore.Files.FileColumns.MIME_TYPE,
-        )
-
-        val sortOrder = MediaStore.Files.FileColumns.DATE_ADDED + " DESC"
-
-        val selection = MediaStore.Files.FileColumns.MIME_TYPE + " = ?"
-
-        val mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension("pdf")
-        val selectionArgs = arrayOf(mimeType)
-
-        val collection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            MediaStore.Files.getContentUri(MediaStore.VOLUME_EXTERNAL)
-        } else {
-            MediaStore.Files.getContentUri("external")
-        }
-
-
-        contentResolver.query(collection, projection, selection, selectionArgs, sortOrder)
-            .use { cursor ->
-                checkNotNull(cursor)
-                if (cursor.moveToFirst()) {
-                    val columnData = cursor.getColumnIndex(MediaStore.Files.FileColumns.DATA)
-                    val columnName =
-                        cursor.getColumnIndex(MediaStore.Files.FileColumns.DISPLAY_NAME)
-                    do {
-                        pdfList.add((cursor.getString(columnData)))
-                        Log.d("PDF", "getPdf: " + cursor.getString(columnData))
-                        //you can get your pdf files
-                    } while (cursor.moveToNext())
-                }
+// Initialize the launcher in onCreate or during initialization
+        openFileLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val file = result.data?.data?.toFile(this)
+                val text = file?.readText().orEmpty()
+                Log.d("hihi", text)
+                // Handle the URI as needed
             }
-        return pdfList
+        }
+        // Запрашиваем разрешения и извлекаем файлы, если разрешение предоставлено
+        openFile()
     }
 
-    private fun fetchPdfFiles() {
-        val pdfFiles = pdfFiles()
-        Log.d("hihi", pdfFiles.toArray().contentToString())
-        for (path in pdfFiles) {
-            Log.d("PDF File Path", path)
+    fun Uri.toFile(context: Context): File? {
+        val inputStream = context.contentResolver.openInputStream(this)
+        val tempFile = File.createTempFile("temp", ".pdf")
+        return try {
+            tempFile.outputStream().use { fileOut ->
+                inputStream?.copyTo(fileOut)
+            }
+            tempFile.deleteOnExit()
+            inputStream?.close()
+            tempFile
+        } catch (e: Exception) {
+            null
         }
     }
 
+    private lateinit var openFileLauncher: ActivityResultLauncher<Intent>
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ReadSummarizedTheme {
-        Text(text = "Пример резюме")
+    // Function to open the file
+    fun openFile() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "application/pdf"
+            // Optionally, specify a URI for the file that should appear in the
+            // system file picker when it loads.
+            // putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+        }
+        openFileLauncher.launch(intent)
     }
-}
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        ReadSummarizedTheme {
+            Text(text = "Пример резюме")
+        }
+    }
 }
